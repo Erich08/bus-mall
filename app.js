0697b47571cd93fa3c8f1434ad5d1c50e04647e5
Erich Hartnauer <erich.hartnauer@gmail.com>
2021-11-03 22:48:17 -0500

    fixing issues

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict';
-// const errorMessage = document.getElementById('h2');
+const clicksAllowed = 25;
 const listResults = document.querySelector('ul');
 const viewResults = document.querySelector('section + div');
 const imgContainer = document.getElementById('container');
@@ -8,22 +8,7 @@ let item2 = document.querySelector('section img:nth-child(2)');
 let item3 = document.querySelector('section img:nth-child(3)');
 let choices = [];
 let clicks = 0;
-const clicksAllowed = 25;
-
-// Display 3 product images
-// Create a constructor function that has the following properties: Name of product, file path, times image has been shown
-// Create algorithm that will randomly generate 3 images and display them side-by-side-by-side
-// For set of 3 images, increment its property of times it has been shown by one
-// Add event listener t othe section of the HTML page where images are
-// On click, generate a set of new images.
-// Inside of the constructor function, define a property to hold the number of times a product has been clicked
-// after each selection by a user, update the newly added property to reflect if it was clicked
-// 25 rounds of voting before session is ended
-// Keep number of rounds in a variable
-// Create a property attached to the constructor that keeps track of all the products that are currently being considered
-// After voting rounds have been completed, remove event listeners
-// Add a button with the text "view results"
-// Displayed product names should match the file name
+let uniqueNumbers = [];
 
 function Items(name, fileExtension = 'jpeg') {
   this.name = name;
@@ -58,13 +43,16 @@ function randomChoice() {
 }
 
 function renderChoices() {
-  let choice1 = randomChoice();
-  let choice2 = randomChoice();
-  let choice3 = randomChoice();
-  while (choice1 === choice2 || choice1 === choice3 || choice2 === choice3) {
-    choice2 = randomChoice();
-    choice3 = randomChoice();
+  while (uniqueNumbers.length < 6) {
+    let rand = randomChoice();
+    if (!uniqueNumbers.includes(rand)) {
+      uniqueNumbers.push(rand);
+    }
   }
+  console.log(uniqueNumbers);
+  let choice1 = uniqueNumbers.shift();
+  let choice2 = uniqueNumbers.shift();
+  let choice3 = uniqueNumbers.shift();
 
   item1.src = choices[choice1].src;
   item1.alt = choices[choice1].name;
@@ -92,19 +80,64 @@ function handleClick(e) {
   renderChoices();
   if (clicks === clicksAllowed) {
     imgContainer.removeEventListener('click', handleClick);
-    viewResults.addEventListener('click', handleResults);
-    viewResults.className = 'clicks-allowed';
+    barGraph();
   }
 }
 
-function handleResults() {
-  for (let i = 0; i < choices.length; i++) {
-    let li = document.createElement('li');
-    li.textContent = `${choices[i].name} had ${choices[i].views} view(s) and was clicked ${choices[i].likes} times.`;
-    listResults.appendChild(li);
-  }
-}
+// function handleResults() {
+//   for (let i = 0; i < choices.length; i++) {
+//     let li = document.createElement('li');
+//     li.textContent = `${choices[i].name} had ${choices[i].views} view(s) and was clicked ${choices[i].likes} times.`;
+//     listResults.appendChild(li);
+//   }
+// }
 
 imgContainer.addEventListener('click', handleClick);
 
 renderChoices();
+
+function barGraph() {
+  let productNames = [];
+  let productViews = [];
+  let productLikes = [];
+  for (let i = 0; i < choices.length; i++) {
+    productNames.push(choices[i].name);
+    productViews.push(choices[i].views);
+    productLikes.push(choices[i].likes);
+  }
+  console.log(productNames);
+  const data = {
+    labels: productNames,
+    datasets: [
+      {
+        label: 'Views',
+        data: productViews,
+        backgroundColor: ['rgba(255, 255, 255)'],
+        borderColor: ['rgb(0, 0, 0)'],
+
+        borderWidth: 1,
+      },
+      {
+        label: 'Likes',
+        data: productLikes,
+        backgroundColor: ['rgba(0, 0, 0)'],
+        borderColor: ['rgb(255, 255, 255)'],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const config = {
+    type: 'bar',
+    data: data,
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  };
+  let canvasChart = document.getElementById('myChart');
+  const myChart = new Chart(canvasChart, config);
+}
